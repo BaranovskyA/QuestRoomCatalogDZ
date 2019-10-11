45815ad406fe131c20d37eaff1277b0a2eaf420e
BaranovskyA <54361221+BaranovskyA@users.noreply.github.com>
2019-10-11 19:25:29 +0600

    Add files via upload

---
+++ QuestRoomCatalog.BusinessLayer/Helpers/QuestRoomsHelper.cs
@@ -0,0 +1,32 @@
+﻿using QuestRoomCatalog.BusinessLayer.BusinessObjects;
+using QuestRoomCatalog.DataLayer;
+using QuestRoomCatalog.DataLayer.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestRoomCatalog.BusinessLayer.Helpers
+{
+    public class QuestRoomsHelper : ICrud<QuestsRoomsBO>
+    {
+        UnitOfWork Db { get; set; }
+
+        public QuestRoomsHelper(UnitOfWork uow)
+        {
+            Db = uow;
+        }
+
+        public void Create(QuestsRoomsBO Bo)
+        {
+            if (Bo.Id == 0)
+            {
+                QuestsRooms questRooms = AutoMapper<QuestsRoomsBO, QuestsRooms>.Map(Bo);
+                Db.QuestsRoomsUowRepository.Add(questRooms);
+                Db.Save();
+            }
+
+        }
+    }
+}
